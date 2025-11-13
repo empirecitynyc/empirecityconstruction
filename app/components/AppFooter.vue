f81810b178a774ffb83c66cf42b3e62718f0a222
Clemucciu <clement.faraud@gmail.com>
2025-11-13 08:13:06 -0600

    footer + other pages

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,23 +1,53 @@
 <script setup lang="ts">
-const { footer } = useAppConfig()
+const columns = [{
+  label: 'DESIGN',
+  children: [{
+    label: 'Permits, Surveys & Estimates'
+  }, {
+    label: '2D Blueprints & 3D Renderings'
+  }]
+}, {
+  label: 'BUILD',
+  children: [{
+    label: 'Structural Work, Waterproofing, Mechanical, Electrical & Plumbing'
+  }, {
+    label: 'Wood Flooring, Stone Fabrication & Tile Work, Plaster & Paint'
+  }]
+}]
 </script>
 
 <template>
-  <UFooter>
+  <USeparator
+    icon="i-lucide-layers"
+    class="h-px"
+  />
+
+  <UFooter :ui="{ top: 'border-b border-default' }">
+    <template #top>
+      <UContainer>
+        <UFooterColumns :columns="columns">
+          <template #right>
+            <div class="text-right">
+              Call (212) 327-1666<br>
+              Fax: (212) 327-1664<br>
+              contact@empirecitynyc.com<br>
+              206 E 67th st, 66 - New York, NY 10065
+            </div>
+          </template>
+        </UFooterColumns>
+      </UContainer>
+    </template>
+
     <template #left>
-      {{ footer.credits }}
+      <p class="text-muted text-sm">
+        Website Built with Nuxt UI
+      </p>
     </template>
 
     <template #right>
-      <UColorModeButton v-if="footer?.colorMode" />
-
-      <template v-if="footer?.links">
-        <UButton
-          v-for="(link, index) of footer?.links"
-          :key="index"
-          v-bind="{ color: 'neutral', variant: 'ghost', ...link }"
-        />
-      </template>
+      <p class="text-muted text-sm">
+        EMPIRE CITY CONSTRUCTION
+      </p>
     </template>
   </UFooter>
 </template>
